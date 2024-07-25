612fb1d20718ceeea457edbbb3405c5dc563f25d
Srejon Khan <srejonkhan79@gmail.com>
2024-07-25 21:53:39 +0600

    feat(server): add express as main http.Server

--- server/src/main.js
+++ server/src/main.js
@@ -1,14 +1,19 @@
-const http = require("http");
+const express = require("express");
 const { argv } = require("process");
 const WebSocketServer = require("websocket").server;
 const { messageHandler, closeHandler } = require("./handlers");
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = Number(argv[2]);
 
-const httpserver = http.createServer();
-const websocket = new WebSocketServer({ httpServer: httpserver });
+const httpServer = app.listen(port, () => {
+  console.log(`Chat WS Server is listening on port ${port}`);
+});
 
-httpserver.listen(port, () => console.log(`Chat WS Server is listening on port ${port}`));
+const websocket = new WebSocketServer({ httpServer: httpServer });
 
 websocket.on("request", (request) => {
   const connection = request.accept(null, request.origin);
