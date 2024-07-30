6a5dc28e7e441796ef418e0d71353f7a1d295bcd
Srejon Khan <srejonkhan79@gmail.com>
2024-07-30 20:17:55 +0600

    feat(server): add cors policy

--- server/src/main.js
+++ server/src/main.js
@@ -1,10 +1,12 @@
 const express = require("express");
+var cors = require("cors");
 const WebSocketServer = require("websocket").server;
 const { messageHandler, closeHandler } = require("./handlers");
 const { uploadMiddleware, uploadController } = require("./upload");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
